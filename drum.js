ae3b3f14ee4c08442b9c63e0c104dd94c92d8617
Indirajithofficial <98870644+Indirajithofficial@users.noreply.github.com>
2023-07-05 10:20:25 +0530

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,27 @@
+//drum kit using jquery....
+
+//detect click..
+$(".btn").click(function(){
+    var id = $(this).attr("id");
+   execute(id);  
+   ani(a); 
+});
+
+//detect keypress..
+$(document).keypress(function(event){
+    execute(event.key);
+    ani(a);
+})
+
+function execute(word){
+
+    var audio = new Audio("./sounds/"+word+".mp3");
+    audio.play();
+    
+//animation
+ $("#"+word).addClass("animate");
+    setTimeout(function(){$("#"+word).removeClass("animate")
+    },150)
+     }
+
+ 
